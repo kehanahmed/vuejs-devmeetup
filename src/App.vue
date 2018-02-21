d7766cad558ddf6cda392deadfa25bee207c8c71
kehanahmed <kehanahmed900@gmail.com>
2018-02-21 19:13:50 +0500

    devmeetup _lectures_1-5_completed

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-navigation-drawer temporary fixed v-model="sideNav">
+      <v-list>
+        <!-- Here router used is the 'directive' -->
+        <v-list-tile 
+        v-for="item in menuItems" 
+        :key="item.title"
+        router
+        :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon @click.native.stop="sideNav=!sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
+        </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <!-- Here router used is the 'directive' -->
+        <v-btn                  
+        flat 
+        v-for="item in menuItems"
+        :key="item.title"
+        router 
+        :to="item.link">
+          <v-icon dark left>{{item.icon}}</v-icon>
+          {{item.title}}
+          </v-btn>                
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      <router-view></router-view>
+   </main>
+  </v-app>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        /* For authentication users */
+       {icon: 'supervisor_account', title: 'View Meetups', link: '/meetups'},
+        {icon: 'room', title: 'Organize Meetup', link: '/meetup/new'},
+        {icon: 'person', title: 'Profile', link: '/profile'},
+        /* For Un-authentication users */
+        {icon: 'face', title: 'Sign up', link: '/signup'},
+        {icon: 'lock_open', title: 'Sign in', link: '/signin'}
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+      <v-layout row wrap>
+          <v-flex x12>
+              <v-card>
+                  <v-card-title>
+                      <h3 class="primary--text">My Meetup</h3>
+                  </v-card-title>
+                  <v-card-media
+                    src= "https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg"
+                    height= "400px"> 
+              </v-card-media>
+              <v-card-text>
+                  <div class="info--text">21st feb 2018 - Where it takes place</div>
+                  <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor nemo corrupti, pariatur dolorem ducimus unde rem soluta quia commodi error consectetur quas excepturi ipsum! Quam mollitia distinctio maiores culpa asperiores.</div>
+              </v-card-text>
+              <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn class="primary">Register</v-btn>
+              </v-card-actions>
+              </v-card>
+          </v-flex>
+      </v-layout>
+  </v-container>
+</template>
